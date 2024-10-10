80bc192b0e9589352e74c47126501615973d98ff
Aasmund Gravem <aasmund.gravem@gmail.com>
2024-10-10 16:37:14 +0200

    add routes for checklists and steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    devise_for :users
+
+    resources :users, only: [ :show ] do
+      resources :checklist_templates do
+        member do
+          post :share
+          post :add_location
+          post :add_image
+        end
+        resources :steps, except: [ :show ]
+      end
+      resources :checklist_instances, only: [ :index, :show, :create ] do
+        member do
+          patch :complete
+        end
+        resources :step_instances, only: [ :update ] do
+          member do
+            patch :add_note
+            patch :add_photo
+          end
+        end
+      end
+      root to: "pages#home"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
